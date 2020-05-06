5a8e88b212794165aaf63eacdca8f501b34c2d41
Wayne Norman <wayne.jt.norman@gmail.com>
2020-05-06 18:47:11 -0400

    Updates

---
+++ precache-manifest.00ce25a425a2a6481565f08b0378fb47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d729156c3ece3937ecba6cca4971b5af",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "16c01543cfb07ea8956a",
+    "url": "/monsters-rolodex/static/css/main.346c4aa8.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c01543cfb07ea8956a",
+    "url": "/monsters-rolodex/static/js/main.69e4b5a2.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
